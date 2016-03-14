4b2e7f912f6c656071ebb8ee69f74e60e61d50c2
ekedziora <ekedziora13@gmail.com>
2016-03-14 21:22:41 +0100

    metody, prawdziwa dokładność, precyzja, recall

--- in.py
+++ in.py
@@ -2,16 +2,33 @@ from nltk.classify.naivebayes import NaiveBayesClassifier
 from nltk.collocations import BigramCollocationFinder
 from nltk.corpus import movie_reviews
 import nltk
-import random
 import string
-from nltk import bigrams
 from nltk.corpus import stopwords
 from nltk.metrics.association import BigramAssocMeasures
+from nltk.metrics.scores import precision, recall
+from collections import defaultdict
+import random
 
 stop = stopwords.words('english')
 
 def normalizeWords(words):
     return [w.lower() for w in words if w.lower() not in stop and w.strip(string.punctuation)]
+
+def findMostFrequentBigrams(words, scoreFunction=BigramAssocMeasures.chi_sq, count=200):
+    bigram_finder = BigramCollocationFinder.from_words(words)
+    return set(bigram_finder.nbest(scoreFunction, count))
+
+def bagOfWordsFeatures(words):
+    all_words_normalized = normalizeWords(words)
+    most_frequent_bigrams = findMostFrequentBigrams(all_words_normalized, count=2000)
+
+    features = {}
+    for unigram in set(all_words_normalized):
+        features['contains({})'.format(unigram)] = True
+    for bigram in most_frequent_bigrams:
+        features['contains({})'.format(bigram)] = True
+
+    return features
 
 def sentimentclassification(trainset, testset):
     (TN, TP, FN, FP) = ( 0 , 0 , 0 , 0)
@@ -31,28 +48,38 @@ def sentimentclassification(trainset, testset):
 
     return ((TN, TP, FN, FP))
 
+def precision_recall(classifier, testFeatures):
+    refsets = defaultdict(set)
+    testsets = defaultdict(set)
+
+    for i, (feats, label) in enumerate(testFeatures):
+        refsets[label].add(i)
+        observed = classifier.classify(feats)
+        testsets[observed].add(i)
+
+    precisions = {}
+    recalls = {}
+
+    for label in classifier.labels():
+        precisions[label] = precision(refsets[label], testsets[label])
+        recalls[label] = recall(refsets[label], testsets[label])
+
+    return precisions, recalls
+
 featureset = []
 
-for fileid in movie_reviews.fileids():
-    all_words_normalized = normalizeWords(movie_reviews.words(fileid))
-    category = movie_reviews.categories(fileid)[0]
+for category in movie_reviews.categories():
+    for fileid in movie_reviews.fileids(category):
+        features = bagOfWordsFeatures(movie_reviews.words(fileid))
+        featureset += [(features, category)]
 
-    bigram_finder = BigramCollocationFinder.from_words(all_words_normalized)
-    most_frequent_bigrams = set(bigram_finder.nbest(BigramAssocMeasures.student_t, 1000))
-
-    features = {}
-    for unigram in set(all_words_normalized):
-        features['contains({})'.format(unigram)] = True
-    for b in most_frequent_bigrams:
-        features['contains({})'.format(b)] = True
-
-    featureset += [(features, category)]
-
-
+random.shuffle(featureset)
 cutpoint = int(len(featureset) * 0.9)
 trainset, testset = featureset[:cutpoint], featureset[cutpoint:]
 
 
 classifier = nltk.NaiveBayesClassifier.train(trainset)
-classifier.show_most_informative_features(20)
-print(nltk.classify.accuracy(classifier, testset))
+print("Accurancy: {}".format(nltk.classify.accuracy(classifier, testset)))
+precisions, recalls = precision_recall(classifier, testset)
+print(precisions)
+print(recalls)
